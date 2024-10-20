385e3ffb1e769a79d912a7210c169bcbbd10f653
dmor1928 <117931725+dmor1928@users.noreply.github.com>
2024-10-20 16:18:32 +0100

    added fat rda content to rda.csv

--- database/cleaning-rda-tables.py
+++ database/cleaning-rda-tables.py
@@ -19,6 +19,8 @@ def cleanColumnTitle(title):
 
     if new_name_split[0] == "vitamin":  # Set name to the vitamin type (e.g. 'vitamin c' --> 'c')
         new_name = new_name_split[1]
+        if new_name == "a":
+            new_name = "retinol"
     elif new_name_split[1] == "equivalent":
         new_name = new_name_split[0]+"_"+new_name_split[1]
     elif new_name_split[0] == "pantothenic":
@@ -27,6 +29,10 @@ def cleanColumnTitle(title):
         new_name = "fibre"
     elif new_name_split[1] == "water":
         new_name = "water"
+    elif new_name_split[0] == "linoleic":
+        new_name = "n_6"
+    elif new_name_split[0] == "alpha-linolenic":
+        new_name = "n_3"
     else:
         new_name = new_name_split[0]  # Else set to first word
     
@@ -64,19 +70,16 @@ def cleanEntry(old_entry):
         return new_entry
 
 
-first_row = ['Age', 'Category', 'Sex'] 
-
+first_row = ['Age', 'Category', 'Sex']  #The column titles
 for file in rda_tables:
     df = pd.read_csv(file)
-    nutrients_columns = df.columns[2:]
+    nutrients_columns_titles = df.columns[2:]
 
-    for column in nutrients_columns:
-        new_column_title = cleanColumnTitle(column)
+    for title in nutrients_columns_titles:
+        new_column_title = cleanColumnTitle(title)
         first_row.append(new_column_title) 
 
-print(first_row)
-
-# The new entries are identical for each table, so only need to perform it once
+# Entries are identical on each table
 table_df = pd.read_csv(rda_tables[0])
 old_entries = table_df.iloc[:, 1]
 new_entries = []
@@ -87,7 +90,7 @@ for entry in old_entries:
     
 print(new_entries)
 
-new_df = pd.DataFrame(columns=first_row)
+compiled_RDA_dataframe = pd.DataFrame(columns=first_row)
 
 for row_index, entry in enumerate(new_entries):
     if type(entry) == type(''):
@@ -118,8 +121,97 @@ for row_index, entry in enumerate(new_entries):
                     clean_nutrient_entry = None
                 new_row.append(clean_nutrient_entry)
 
-        new_df.loc[row_index] = new_row
+        compiled_RDA_dataframe.loc[row_index] = new_row
 
-print(new_df)
 
-new_df.to_csv('./clean-tables/RDA.csv')
+cofid_df = pd.read_csv('./clean-tables/complete_nutritional_data.csv')
+cofid_nutrients = list(cofid_df.columns[2:])
+rda_nutrients = list(compiled_RDA_dataframe.columns[3:])
+
+# print("cofid: ", cofid_nutrients)
+# print("rda   : ", rda_nutrients)
+for element in (set(cofid_nutrients) ^ set(rda_nutrients)):
+    print(element)
+"""
+Current assumptions/approximations: 
+Vitamin K = K1 are the same
+Total fibre = AOAC Fibre
+vitamin A = retinol
+Trans fat maximum daily intake = 0g for everyone
+
+Data missing from complete_nutritional_data but included in RDA.csv:
+chromium
+choline
+fluoride
+molybdenum
+water
+k                           Just say k1 = k
+
+Data missing from RDA.csv but included in complete_nutritional_data:
+## Macros: ##
+energy / calories (this can be chosen by the user?)
+## Fat types: ##
+trans                       Handled by missingFatRDAColumns
+mono                        Will be handled by missingFatRDAColumns in future
+satd                        Handled by missingFatRDAColumns
+poly (polyunsaturated fat)  Will be handled by missingFatRDAColumns in future
+cholesterol                 Handled by missingFatRDAColumns
+## Vitamins: ##
+niacin_equivalent           Just count as niacin
+retinol_equivalent          Just count as retinol
+k1                          Just say k1 = k
+## Carotenes: ##
+carotene
+alpha_carotene
+beta_carotene
+lycopene
+## Carotenoids: ##
+lutein
+## amino acids: ##
+tryptophan_60
+"""
+
+def missingFatRDAColumns (age_column):
+    # Add missing fat RDA to the 'fat' column
+    total_fat_column = pd.Series()
+    for row_index, age in enumerate(age_column):
+        if age < 1:
+            total_fat_column[row_index] = 30
+        elif 1 <= age < 4:
+            total_fat_column[ row_index] = 39
+        else:
+            total_fat_column[row_index] = 78
+        print(age,", ", total_fat_column[row_index])
+
+    saturated_fat_column = pd.Series()
+    for row_index, age in enumerate(age_column):
+        if age <= 1:
+            saturated_fat_column[row_index] = None
+        elif 1 < age <= 4:
+            saturated_fat_column[row_index] = 10
+        else:
+            saturated_fat_column[row_index] = 20
+
+    cholesterol_column = pd.Series()
+    for row_index, age in enumerate(age_column):
+        if age <= 1:
+            cholesterol_column[row_index] = None
+        else:
+            cholesterol_column[row_index] = 300
+    
+    trans_fat_column = pd.Series()
+    for row_index, age in enumerate(age_column):
+        trans_fat_column[row_index] = 0
+    
+    return total_fat_column, saturated_fat_column, cholesterol_column, trans_fat_column
+
+fat, satd, cholesterol, trans = missingFatRDAColumns(compiled_RDA_dataframe['Age'])
+
+compiled_RDA_dataframe = compiled_RDA_dataframe.assign(fat=fat.values)
+compiled_RDA_dataframe = compiled_RDA_dataframe.assign(satd=satd.values)
+compiled_RDA_dataframe = compiled_RDA_dataframe.assign(cholesterol=cholesterol.values)
+compiled_RDA_dataframe = compiled_RDA_dataframe.assign(trans=trans.values)
+
+
+
+compiled_RDA_dataframe.to_csv('./clean-tables/RDA.csv')
