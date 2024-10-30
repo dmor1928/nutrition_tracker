acde34c05cc6c5a70a967bc92ec6fbd89a0f26db
dmor1928 <117931725+dmor1928@users.noreply.github.com>
2024-10-30 23:20:10 +0000

    Refactoring the RDA for __init__. Added a couple more functions which shall replace addRDADefaults

--- website/__init__.py
+++ website/__init__.py
@@ -23,7 +23,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note, Recipe, RecipeIngredient, RDADefault#, Foods
+    from .models import User, Note, Recipe, RecipeIngredient, RDAProfile, RDAValues, UserPersonalRDA#, Foods
    #  from .fdc_models import FDCFood, FDCFoodNutrition, FDCFoodPortion#, FDCFoodPreparationOptions, FDCFoodPreparationFactors
 
     create_database(app)
@@ -49,24 +49,27 @@ def create_database(app):
     instance_dir += "\\instance\\"
 
     if not path.exists(instance_dir + DB_NAME):
-        with app.app_context():
-            db.create_all()
-        print('Created Database')
 
-         # Importing csv data to models
-        from .models import User
         engine = db.create_engine('sqlite:///instance/database.db', echo=False)
         Base = declarative_base()
         Base.metadata.create_all(engine)
-        cofid_file_dir = 'database/clean-tables/'
 
-        # addCoFID(engine, cofid_file_dir)
-        # addRDADefaults(engine, cofid_file_dir)
-
+        with app.app_context():
+            db.create_all()
+        
+        print('Created New Database')
+        # Importing csv data to models
         fdc_file_dir = 'fdc-database/fdc-reduced/'
-
         addFDC(engine, fdc_file_dir)
 
+        rda_profiles_dir = 'database/clean-tables/rda-profiles/'
+        addRDAProfiles(engine, rda_profiles_dir)
+
+        rda_values_dir = 'database/clean-tables/rda-values/'
+        addRDAValues(engine, rda_values_dir)
+
+        # Adding sample user
+        from .models import User
         import datetime
         from dateutil.relativedelta import relativedelta
         from werkzeug.security import generate_password_hash
@@ -74,23 +77,32 @@ def create_database(app):
         birthDate = datetime.datetime(2002, 8, 15)
         today = datetime.date.today()
         age = relativedelta(today, birthDate).years
-        rda_id = 7
+        rda_id = 11
 
         sample_user = User(
             email='david@gmail.com', 
             password=generate_password_hash('password123', method='pbkdf2:sha256'), 
             firstName='David', 
+
             birthDate=birthDate,
             age=age,
             sex='male',
+            weight_kg=63,
             isPregnant = False,
             isLactating = False,
-            RDADefault_id = rda_id,
+            # RDADefault_id = rda_id,
+            rda_profile_id = rda_id,
             isCustomRDA = False)
-        
+
         with app.app_context():
             db.session.add(sample_user)
             db.session.commit()
+    
+    # cofid_file_dir = 'database/clean-tables/'
+    # addCoFID(engine, cofid_file_dir)
+    # addRDADefaults(engine, cofid_file_dir) # Updates RDADefault based on RDA_defaults.csv table every launch
+        
+        
 
 def addCoFID(engine, cofid_file_dir):
     from .models import Foods, NutrientUnit
@@ -103,12 +115,11 @@ def addCoFID(engine, cofid_file_dir):
 
     print("Imported CoFID food data")
 
-def addRDADefaults(engine, rda_file_dir):
-    from .models import RDADefault
-    df = pd.read_csv(rda_file_dir + 'RDA_defaults.csv')
-    df.to_sql(con=engine, name=RDADefault.__tablename__, if_exists='replace', index=False)
-
-    print("Imported default RDA data")
+# def addRDADefaults(engine, rda_file_dir):
+#     from .models import RDADefault
+#     df = pd.read_csv(rda_file_dir + 'RDA_defaults.csv')
+#     df.to_sql(con=engine, name=RDADefault.__tablename__, if_exists='replace', index=False)
+#     print("Imported default RDA data")
 
 def addFDC(engine, FDC_file_dir):
     from .fdc_models import FDCFood, FDCFoodNutrition, FDCNutrients#, FDCFoodPortion, FDCFoodPreparationOptions, FDCFoodPreparationFactors
@@ -129,3 +140,15 @@ def addFDC(engine, FDC_file_dir):
     # df.to_sql(con=engine, name=FDCFoodPreparationFactors.__tablename__, if_exists='replace', index=False)
 
     print("Imported FDC food, nutrients and portion data")
+
+def addRDAProfiles(engine, rda_files_dir):
+    from .models import RDAProfile
+    df = pd.read_csv(rda_files_dir + 'rda_profiles.csv')
+    df.to_sql(con=engine, name=RDAProfile.__tablename__, if_exists='replace', index=False)
+    print("Imported RDAProfile")
+
+def addRDAValues(engine, rda_files_dir):
+    from .models import RDAValues
+    df = pd.read_csv(rda_files_dir + 'rda_values.csv')
+    df.to_sql(con=engine, name=RDAValues.__tablename__,  if_exists='replace', index=False)
+    print("Imported RDAValues")
