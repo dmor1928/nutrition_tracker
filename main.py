15154d9d75dee16120d2bf7570a4461451a0221d
dmor1928 <117931725+dmor1928@users.noreply.github.com>
2024-11-20 23:49:42 +0000

    unnecessary comments

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':  # Ensures it doesn't run if this is imported anywhere
-    app.run(debug=True)  # Automatically updates the webserver when changes are made
+if __name__ == '__main__':
+    app.run(debug=True)
 
